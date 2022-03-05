93a1c56dee4dbe9ff6d3dd66960b580efd68cf95
Subash Prakash <subashprakash8@gmail.com>
2022-03-05 12:32:14 +0100

    Project-1 Initial work Addition

---
+++ etl.py
@@ -0,0 +1,193 @@
+from doctest import UnexpectedException
+import os
+import glob
+import psycopg2
+from psycopg2.errors import UniqueViolation
+import pandas as pd
+from sql_queries import *
+from datetime import datetime
+
+
+def clean_data(df, na_subset, dup_subset=None):
+    """This function cleans the dataframe with following the Normalization rules:
+    1. Data Integrity by droping duplicates on ID.
+    2. There are some '' ids those are turned to NaN and dropped for ids through droping_duplicates.
+    3. Drop na on ids
+    The imputations on the length is not done. If required can be added here.
+    Args:
+        [df] -> It is the dataframe object under consideration
+        [na_subset] -> It is the list of columns where na should be removed
+        [dup_set] -> It is the list of columns wherer the duplicates should not be considered. Ideally the id columns
+        returns cleaned dataframe
+    """
+    # Common cleaning to replace '' with NaN on ids
+    temp_df = df.copy()
+    temp_df.dropna(subset=na_subset, inplace=True)
+    temp_df.drop_duplicates(subset=dup_subset, inplace=True)
+    return temp_df
+
+
+def write_duplicate_records(data):
+    """
+    Writing the duplicate records to a file to keep track of ids which are of duplication.
+    Args:
+        (data)(List): List of data.
+    """
+    with open(f"duplicate_records.txt", "a+") as f:
+        f.write(f"[INFO][{datetime.utcnow()}] - {','.join(str(item) for item in data)} \n")
+
+
+def process_song_file(cur, filepath):
+    """Process the song file and insert into to database at artist and song table.
+    Note: We need to insert first to the artist table and then to song table avoiding
+    foreign key violation.
+    Args:
+        cur (_type_): The connection cursor object
+        filepath (str): The filepath of the song data
+    """
+    try:
+        # open song file
+        df = pd.read_json(filepath, lines=True)
+
+        # insert artist record
+        artist_data = df[
+            [
+                "artist_id",
+                "artist_name",
+                "artist_location",
+                "artist_latitude",
+                "artist_longitude",
+            ]
+        ]
+        insertion_data = artist_data.values.tolist()[0]
+        cur.execute(artist_table_insert, insertion_data)
+
+        # insert song record
+        song_data = df[["song_id", "title", "artist_id", "year", "duration"]]
+        insertion_data = song_data.values.tolist()[0]
+        cur.execute(song_table_insert, insertion_data)
+    except UniqueViolation as uexc:
+        print(uexc)
+        print("The data has already an entry in the db")
+        print(f"Data - {insertion_data}")
+        write_duplicate_records(insertion_data)
+
+
+def process_log_file(cur, filepath):
+    """This function process the log data file and writes to timestamp table,
+    user table and songplay finally.
+
+    Args:
+        cur (_type_): _description_
+        filepath (str): The log data file.
+    """
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df["page"] == "NextSong"]
+
+    # convert timestamp column to datetime
+    t = df.copy()
+    t["ts"] = t["ts"].apply(lambda x: pd.to_datetime(x, unit="ms"))
+    t["hour"] = t["ts"].apply(lambda x: x.hour)
+    t["day"] = t["ts"].apply(lambda x: x.day)
+    t["week"] = t["ts"].apply(lambda x: x.week)
+    t["month"] = t["ts"].apply(lambda x: x.month)
+    t["year"] = t["ts"].apply(lambda x: x.year)
+    t["weekday"] = t["ts"].apply(lambda x: x.dayofweek)
+    time_df = t[["ts", "hour", "day", "week", "month", "year", "weekday"]]
+    time_df = clean_data(time_df, na_subset=["ts"], dup_subset=["ts"])
+
+    for i, row in time_df.iterrows():
+        try:
+            cur.execute(time_table_insert, list(row))
+        except UniqueViolation as uxc:
+            print(uxc)
+            print(list(row))
+            write_duplicate_records(row)
+            continue
+        except Exception as e:
+            print(e)
+            continue
+
+    # load user table
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
+    user_df = clean_data(user_df, na_subset=["userId"], dup_subset=["userId"])
+    # insert user records
+    for i, row in user_df.iterrows():
+        try:
+            cur.execute(user_table_insert, row)
+        except UniqueViolation as uxc:
+            print(uxc)
+            print(list(row))
+            write_duplicate_records(row)
+            continue
+        except Exception as e:
+            print(e)
+            continue
+    # insert songplay records
+    for index, row in df.iterrows():
+        try:
+            # get songid and artistid from song and artist tables
+            cur.execute(song_select, (row.song, row.artist, row.length))
+            results = cur.fetchone()
+
+            if results:
+                songid, artistid = results
+            else:
+                songid, artistid = None, None
+
+            # insert songplay record
+            songplay_data = (
+                pd.to_datetime(row.ts, unit="ms"),
+                row.userId,
+                row.level,
+                songid,
+                artistid,
+                row.sessionId,
+                row.location,
+                row.userAgent,
+            )
+            cur.execute(songplay_table_insert, songplay_data)
+        except Exception as e:
+            print(e)
+            print(list(row))
+            break
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print("{} files found in {}".format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print("{}/{} files processed.".format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
+    )
+    # Need to avoid on violations
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,113 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+# Cannot simply use user -> it is a reserved keyword
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays(
+    songplay_id SERIAL NOT NULL PRIMARY KEY,
+    start_time timestamp,
+    user_id int,
+    level varchar,
+    song_id varchar,
+    artist_id varchar,
+    session_id varchar,
+    location varchar,
+    user_agent text,
+    CONSTRAINT FK_start_time FOREIGN KEY(start_time) REFERENCES time(start_time),
+    CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_song_id FOREIGN KEY(song_id) REFERENCES songs(song_id),
+    CONSTRAINT FK_artist_id FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
+);
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id int NOT NULL PRIMARY KEY,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar
+);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs(
+    song_id varchar NOT NULL PRIMARY KEY,
+    title varchar,
+    artist_id varchar,
+    year int,
+    duration float,
+    CONSTRAINT FK_artist_id FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
+);
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists(
+    artist_id varchar NOT NULL PRIMARY KEY,
+    name varchar,
+    location varchar,
+    latitude float,
+    longitude float
+);
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time(
+    start_time timestamp NOT NULL PRIMARY KEY,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday int
+);
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplays(start_time,user_id,
+                     level,song_id,artist_id,session_id,location,user_agent)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+""")
+
+user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level)
+VALUES (%s,%s,%s,%s,%s)
+""")
+
+song_table_insert = ("""
+INSERT INTO songs(song_id, title, artist_id, year, duration)
+VALUES (%s,%s,%s,%s,%s)
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists(artist_id, name, location, latitude, longitude)
+VALUES (%s,%s,%s,%s,%s)
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time(start_time, hour, day, week, month, year, weekday)
+VALUES (%s,%s,%s,%s,%s,%s,%s)
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT songs.song_id, artists.artist_id FROM songs
+JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title = %s and artists.name = %s and songs.duration = %s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
