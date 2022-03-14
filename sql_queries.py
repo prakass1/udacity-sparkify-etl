28a23b72866bf0344604bdfdaf5638d506efe498
Subash Prakash <subashprakash8@gmail.com>
2022-03-14 19:49:01 +0100

    Add EXCLUDED to update on conflict to new value

--- sql_queries.py
+++ sql_queries.py
@@ -77,12 +77,12 @@ INSERT INTO songplays(start_time,user_id,
                      level,song_id,artist_id,session_id,location,user_agent)
 VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
 """)
-
+# EXCLUDED along with column is required to be added on conflict. If not then table itself is okay.
 user_table_insert = ("""
 INSERT INTO users(user_id, first_name, last_name, gender, level)
 VALUES (%s,%s,%s,%s,%s)
 ON CONFLICT (user_id)
-DO UPDATE SET level=users.level
+DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
